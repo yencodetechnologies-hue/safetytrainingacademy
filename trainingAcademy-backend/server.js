d1aef1774275a6ef554e9d2a4d57bade31ff059d
jozva <kingsonjozva@gmail.com>
2026-04-15 18:21:26 +0530

    listen

--- trainingAcademy-backend/server.js
+++ trainingAcademy-backend/server.js
@@ -47,6 +47,6 @@ app.use("/api/booking-email", bookingEmailRoutes);
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
