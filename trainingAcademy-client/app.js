b2ef7ac54c8801e750de7f6b1893ca0609aaa71a
jozva <kingsonjozva@gmail.com>
2026-04-19 11:05:13 +0530

    img-height

--- trainingAcademy-client/app.js
+++ trainingAcademy-client/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000"
+const API_URL = "https://api.octosofttechnologies.in"
 
 async function loadForm() {
     const params = new URLSearchParams(window.location.search)
